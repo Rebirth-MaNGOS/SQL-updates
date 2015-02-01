82e72a41791311293eb02ef7347bdf02f64ea9eb
Revontulet <revontulet@nonexistent.fake>
2015-02-01 22:21:25 +0100

    PyrewoodVIllage updated timer again

--- SQL-post-2015-01-31.sql
+++ SQL-post-2015-01-31.sql
@@ -1,3 +1,3 @@
 /************* Updated timers for Pyrewood Village event *****************/
-UPDATE `zp_mangosd`.`game_event` SET `length`=840 WHERE  `entry`=50;
+UPDATE `zp_mangosd`.`game_event` SET `length`=900 WHERE  `entry`=50;
 UPDATE `zp_mangosd`.`game_event` SET `length`=540 WHERE  `entry`=51;
