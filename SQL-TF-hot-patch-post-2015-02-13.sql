8e18036e132ac1173fde354d3469206975dddd9f
andkem <kempe@lysator.liu.se>
2015-02-16 23:14:07 +0100

    Added Muggle's SQL for the Cluster Rockets to be run at the TF hot-patch.

---
+++ SQL-TF-hot-patch-post-2015-02-13.sql
@@ -0,0 +1,4 @@
+-- Large Cluster Rocket Recipes
+REPLACE INTO `zp_mangosd`.`item_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`, `condition_value1`, `condition_value2`) VALUES ('21743', '21733', '100', '0', '1', '1', '0', '0', '0');
+REPLACE INTO `zp_mangosd`.`item_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`, `condition_value1`, `condition_value2`) VALUES ('21743', '21734', '100', '0', '1', '1', '0', '0', '0');
+REPLACE INTO `zp_mangosd`.`item_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`, `condition_value1`, `condition_value2`) VALUES ('21743', '21735', '100', '0', '1', '1', '0', '0', '0');
