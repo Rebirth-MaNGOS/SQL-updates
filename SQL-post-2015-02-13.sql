b79ba1b9ef71640ac4b3fb2a2c10c7ac9dfcb106
Revontulet <revontulet@nonexistent.fake>
2015-03-02 10:59:45 +0100

    SQL for Therylune's Escape

--- SQL-post-2015-02-13.sql
+++ SQL-post-2015-02-13.sql
@@ -483,3 +483,34 @@ UPDATE `zp_mangosd`.`creature_ai_scripts` SET `event_type`=0, `event_param2`=150
 -- Add GO light for Priest Epic Quest
 REPLACE INTO `gameobject` VALUES (1712726, 179693, 0, 3339.37, -2977.07, 162.696, 0.530169, 0, 0, 0.261991, 0.96507, -900, 100, 1);
 
+/********************** Therylune's Escape ***********************/
+-- Add texts and AI
+REPLACE INTO `zp_scriptdevzerod`.`script_texts` VALUES (-1720008, 'Ok, let\'s go!', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 0, 0, 0, 'Therylune, Say Start');
+REPLACE INTO `zp_scriptdevzerod`.`script_texts` VALUES (-1720009, 'I can make it the rest of the way. $N. THANKS!', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 0, 0, 0, 'Therylune, Say End');
+UPDATE `zp_mangosd`.`creature_template` SET `ScriptName`='npc_therylune' WHERE  `entry`=3584;
+
+-- Add wp
+REPLACE INTO `zp_scriptdevzerod`.`script_waypoint` VALUES (3584, 0, 4520.4, 420.235, 33.5284, 2000, '');
+REPLACE INTO `zp_scriptdevzerod`.`script_waypoint` VALUES (3584, 1, 4512.26, 408.881, 32.9308, 0, '');
+REPLACE INTO `zp_scriptdevzerod`.`script_waypoint` VALUES (3584, 2, 4507.94, 396.47, 32.9476, 0, '');
+REPLACE INTO `zp_scriptdevzerod`.`script_waypoint` VALUES (3584, 3, 4507.53, 383.781, 32.995, 0, '');
+REPLACE INTO `zp_scriptdevzerod`.`script_waypoint` VALUES (3584, 4, 4512.1, 374.02, 33.166, 0, '');
+REPLACE INTO `zp_scriptdevzerod`.`script_waypoint` VALUES (3584, 5, 4519.75, 373.241, 33.1574, 3000, '');
+REPLACE INTO `zp_scriptdevzerod`.`script_waypoint` VALUES (3584, 6, 4592.41, 369.127, 31.4893, 0, '');
+REPLACE INTO `zp_scriptdevzerod`.`script_waypoint` VALUES (3584, 7, 4598.55, 364.801, 31.4947, 0, '');
+REPLACE INTO `zp_scriptdevzerod`.`script_waypoint` VALUES (3584, 8, 4602.76, 357.649, 32.9265, 0, '');
+REPLACE INTO `zp_scriptdevzerod`.`script_waypoint` VALUES (3584, 9, 4597.88, 352.629, 34.0317, 2000, '');
+REPLACE INTO `zp_scriptdevzerod`.`script_waypoint` VALUES (3584, 10, 4590.23, 350.9, 36.2977, 0, '');
+REPLACE INTO `zp_scriptdevzerod`.`script_waypoint` VALUES (3584, 11, 4581.5, 348.254, 38.3878, 0, '');
+REPLACE INTO `zp_scriptdevzerod`.`script_waypoint` VALUES (3584, 12, 4572.05, 348.059, 42.3539, 0, '');
+REPLACE INTO `zp_scriptdevzerod`.`script_waypoint` VALUES (3584, 13, 4564.75, 344.041, 44.2463, 0, '');
+REPLACE INTO `zp_scriptdevzerod`.`script_waypoint` VALUES (3584, 14, 4556.63, 341.003, 47.6755, 0, '');
+REPLACE INTO `zp_scriptdevzerod`.`script_waypoint` VALUES (3584, 15, 4554.38, 334.968, 48.8003, 0, '');
+REPLACE INTO `zp_scriptdevzerod`.`script_waypoint` VALUES (3584, 16, 4557.63, 329.783, 49.9532, 0, '');
+REPLACE INTO `zp_scriptdevzerod`.`script_waypoint` VALUES (3584, 17, 4563.32, 316.829, 53.2409, 0, '');
+REPLACE INTO `zp_scriptdevzerod`.`script_waypoint` VALUES (3584, 18, 4566.09, 303.127, 55.0396, 0, '');
+REPLACE INTO `zp_scriptdevzerod`.`script_waypoint` VALUES (3584, 19, 4561.65, 295.456, 57.0984, 4000, 'SAY_THERYLUNE_FINISH');
+REPLACE INTO `zp_scriptdevzerod`.`script_waypoint` VALUES (3584, 20, 4505.03, 279.3, 57.9, 2000, 'DESPAWN');
+
+-- don't use old scripts
+UPDATE `zp_mangosd`.`quest_template` SET `StartScript`=0 WHERE  `entry`=945;
