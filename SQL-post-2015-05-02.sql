71822f9600b45f70fb602979caede16a6f186776
Revontulet <revontulet@nonexistent.fake>
2015-06-06 14:37:50 +0200

    Mistress natalia added AI, armor and adjusted attack speed

--- SQL-post-2015-05-02.sql
+++ SQL-post-2015-05-02.sql
@@ -1,2 +1,10 @@
 -- remove low lvl loot from blackhand veteran's loot table
 DELETE FROM `zp_mangosd`.`creature_loot_template` WHERE  `entry`=9819 AND `item`=24075;
+
+-- ai and armor for Mistress Natalia Mar'alith
+UPDATE `zp_mangosd`.`creature_template` SET `armor`=2950, `baseattacktime`=1600, `AIName`='', `ScriptName`='npc_mistress_natalia_maralith' WHERE  `entry`=15215;
+
+-- add text
+REPLACE INTO `script_texts` VALUES (-1720120, 'C\'Thun will retake this world!', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 0, 0, 0, 'Mistress - Say on Death');
+REPLACE INTO `script_texts` VALUES (-1720119, 'You will make a fitting sacrifice!', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 0, 0, 0, 'Mistress - Say on Aggro');
+
