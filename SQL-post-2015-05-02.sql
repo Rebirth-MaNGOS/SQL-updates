044a350533e42c5415ec3f93652a4920516c2561
andkem <kempe@lysator.liu.se>
2015-07-04 22:06:36 +0200

    Moved the Colossal Anubisath AI SQL so it won't be overwritten.

--- SQL-post-2015-05-02.sql
+++ SQL-post-2015-05-02.sql
@@ -1596,8 +1596,6 @@ UPDATE `zp_mangosd`.`creature_template` SET `speed_run`=2.1 WHERE  `entry`=15742
 REPLACE INTO `zp_mangosd`.`creature_template` (`entry`, `modelid_1`, `modelid_2`, `name`, `minhealth`, `maxhealth`, `minmana`, `maxmana`, `faction_A`, `faction_H`, `speed_walk`, `speed_run`, `scale`, `mindmg`, `maxdmg`, `attackpower`, `baseattacktime`, `unit_class`, `unit_flags`, `type`, `flags_extra`, `ScriptName`) VALUES (17091, 1126, 13069, 'Ahn\'Qiraj Gate Trigger', 8, 8, 100, 100, 35, 35, 1.1, 0.95, 0, 2, 2, 1, 2000, 1, 33555200, 10, 130, 'npc_ahnqiraj_gate_trigger');
 REPLACE INTO `zp_mangosd`.`creature` (`guid`, `id`, `map`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `DeathState`, `MovementType`) VALUES (12787, 17091, 1, 0, 0, -8108.13, 1524.25, 2.60941, 0.14046, 25, 5, 0, 8, 100, 0, 0);
 
--- Colossal Anubisath AI
-UPDATE `zp_mangosd`.`creature_template` SET `ScriptName`='npc_colossal_anubisath' WHERE  `entry`=15743;
 
 -- Qiraji resonating crystal formations
 
@@ -1654,6 +1652,9 @@ REPLACE INTO `zp_mangosd`.`creature` (`guid`, `id`, `map`, `modelid`, `equipment
 REPLACE INTO `zp_mangosd`.`creature` (`guid`, `id`, `map`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `DeathState`, `MovementType`) VALUES (7753, 15751, 1, 0, 0, -6588.1, -3664.65, -58.7498, 5.16401, 25, 5, 0, 14256, 0, 0, 1);
 REPLACE INTO `zp_mangosd`.`creature` (`guid`, `id`, `map`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `DeathState`, `MovementType`) VALUES (7754, 15751, 1, 0, 0, -6566.25, -3661.81, -58.7498, 3.20602, 25, 5, 0, 14256, 0, 0, 1);
 REPLACE INTO `zp_mangosd`.`creature` (`guid`, `id`, `map`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `DeathState`, `MovementType`) VALUES (7755, 15751, 1, 0, 0, -6579.3, -3647.12, -58.7498, 3.81077, 25, 5, 0, 14784, 0, 0, 1);
+
+-- Colossal Anubisath AI
+UPDATE `zp_mangosd`.`creature_template` SET `ScriptName`='npc_colossal_anubisath' WHERE  `entry`=15743;
 
 -- Colossal Anubisath pathing
 REPLACE INTO `zp_scriptdevzerod`.`script_waypoint` (`entry`, `pointid`, `location_x`, `location_y`, `location_z`, `waittime`, `point_comment`) VALUES (15743, 1, -8067.43, 1536.56, 2.61044, 0, NULL);
