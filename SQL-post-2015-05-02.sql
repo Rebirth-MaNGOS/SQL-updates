1456e23e556452ed817adda416cec52032eb23c2
Revontulet <revontulet@nonexistent.fake>
2015-07-24 22:13:01 +0200

    Witherbark protector hitbox, Hakkari urn should now have 100% drop on all 3 items, voodoo dolls should no longer drop from mobs

--- SQL-post-2015-05-02.sql
+++ SQL-post-2015-05-02.sql
@@ -4271,3 +4271,22 @@ REPLACE INTO `zp_scriptdevzerod`.`script_texts` VALUES (-1720194, 'HAKKAR LIVES!
 
 REPLACE INTO `zp_mangosd`.`item_template` (`entry`, `class`, `subclass`, `name`, `displayid`, `Quality`, `Flags`, `BuyCount`, `BuyPrice`, `SellPrice`, `InventoryType`, `AllowableClass`, `AllowableRace`, `ItemLevel`, `RequiredLevel`, `RequiredSkill`, `RequiredSkillRank`, `requiredspell`, `requiredhonorrank`, `RequiredCityRank`, `RequiredReputationFaction`, `RequiredReputationRank`, `maxcount`, `stackable`, `ContainerSlots`, `stat_type1`, `stat_value1`, `stat_type2`, `stat_value2`, `stat_type3`, `stat_value3`, `stat_type4`, `stat_value4`, `stat_type5`, `stat_value5`, `stat_type6`, `stat_value6`, `stat_type7`, `stat_value7`, `stat_type8`, `stat_value8`, `stat_type9`, `stat_value9`, `stat_type10`, `stat_value10`, `dmg_min1`, `dmg_max1`, `dmg_type1`, `dmg_min2`, `dmg_max2`, `dmg_type2`, `dmg_min3`, `dmg_max3`, `dmg_type3`, `dmg_min4`, `dmg_max4`, `dmg_type4`, `dmg_min5`, `dmg_max5`, `dmg_type5`, `armor`, `holy_res`, `fire_res`, `nature_res`, `frost_res`, `shadow_res`, `arcane_res`, `delay`, `ammo_type`, `RangedModRange`, `spellid_1`, `spelltrigger_1`, `spellcharges_1`, `spellppmRate_1`, `spellcooldown_1`, `spellcategory_1`, `spellcategorycooldown_1`, `spellid_2`, `spelltrigger_2`, `spellcharges_2`, `spellppmRate_2`, `spellcooldown_2`, `spellcategory_2`, `spellcategorycooldown_2`, `spellid_3`, `spelltrigger_3`, `spellcharges_3`, `spellppmRate_3`, `spellcooldown_3`, `spellcategory_3`, `spellcategorycooldown_3`, `spellid_4`, `spelltrigger_4`, `spellcharges_4`, `spellppmRate_4`, `spellcooldown_4`, `spellcategory_4`, `spellcategorycooldown_4`, `spellid_5`, `spelltrigger_5`, `spellcharges_5`, `spellppmRate_5`, `spellcooldown_5`, `spellcategory_5`, `spellcategorycooldown_5`, `bonding`, `description`, `PageText`, `LanguageID`, `PageMaterial`, `startquest`, `lockid`, `Material`, `sheath`, `RandomProperty`, `block`, `itemset`, `MaxDurability`, `area`, `Map`, `BagFamily`, `ScriptName`, `DisenchantID`, `FoodType`, `minMoneyLoot`, `maxMoneyLoot`, `Duration`, `ExtraFlags`) VALUES (160, 2, 4, 'Mace of Vek\'lor', 34190, 6, 0, 1, 290604, 72651, 23, 32767, -1, 81, 60, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 5, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1234, 56789, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, 0, 0, 0, 1, '', 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'internalitemhandler', 65, 0, 0, 0, 0, 0);
 REPLACE INTO `zp_mangosd`.`creature_equip_template` (`entry`, `equipentry1`, `equipentry2`, `equipentry3`) VALUES (1337, 160, 0, 0);
+
+-- Hakkari Urn, 100% drop on all items
+UPDATE `zp_mangosd`.`item_loot_template` SET `ChanceOrQuestChance`=100 WHERE  `entry`=10773 AND `item`=10780;
+UPDATE `zp_mangosd`.`item_loot_template` SET `ChanceOrQuestChance`=100 WHERE  `entry`=10773 AND `item`=10781;
+UPDATE `zp_mangosd`.`item_loot_template` SET `ChanceOrQuestChance`=100 WHERE  `entry`=10773 AND `item`=10782;
+
+-- no mobs should be allowed to drop voodoo dolls in ZG
+DELETE FROM `zp_mangosd`.`creature_loot_template` WHERE `item`=19813;
+DELETE FROM `zp_mangosd`.`creature_loot_template` WHERE `item`=19814;
+DELETE FROM `zp_mangosd`.`creature_loot_template` WHERE `item`=19815;
+DELETE FROM `zp_mangosd`.`creature_loot_template` WHERE `item`=19816;
+DELETE FROM `zp_mangosd`.`creature_loot_template` WHERE `item`=19817;
+DELETE FROM `zp_mangosd`.`creature_loot_template` WHERE `item`=19818;
+DELETE FROM `zp_mangosd`.`creature_loot_template` WHERE `item`=19819;
+DELETE FROM `zp_mangosd`.`creature_loot_template` WHERE `item`=19820;
+DELETE FROM `zp_mangosd`.`creature_loot_template` WHERE `item`=19821;
+
+-- Witherbark Protector hitbox
+UPDATE `zp_mangosd`.`creature_model_info` SET `bounding_radius`=2, `combat_reach`=2 WHERE  `modelid`=12929;
