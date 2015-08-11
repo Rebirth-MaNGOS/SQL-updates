0522820deb6e34b454798dfb15ba757e5af006f8
Revontulet <revontulet@nonexistent.fake>
2015-08-11 11:17:45 +0200

    Eye tentacle and Tentacle portal for C'thun adjusted model

---
+++ SQL-post-2015-08-05.sql
@@ -0,0 +1,3 @@
+-- Eye tentacle correct model, tentacle portal change back to right model
+UPDATE `zp_mangosd`.`creature_template` SET `modelid_1`=15882 WHERE  `entry`=15904;
+UPDATE `zp_mangosd`.`creature_template` SET `modelid_1`=15788 WHERE  `entry`=15726;
