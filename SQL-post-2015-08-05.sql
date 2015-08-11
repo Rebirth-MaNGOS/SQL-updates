4bebca60156d1d7f69c51ff90a60dfe2d5bcb69e
Revontulet <revontulet@nonexistent.fake>
2015-08-11 11:53:41 +0200

    Jubling model updated, sql from muggle

--- SQL-post-2015-08-05.sql
+++ SQL-post-2015-08-05.sql
@@ -1,3 +1,6 @@
 -- Eye tentacle correct model, tentacle portal change back to right model
 UPDATE `zp_mangosd`.`creature_template` SET `modelid_1`=15882 WHERE  `entry`=15904;
 UPDATE `zp_mangosd`.`creature_template` SET `modelid_1`=15788 WHERE  `entry`=15726;
+
+-- SQL from muggle, jubling corrected model
+UPDATE `zp_mangosd`.`creature_template` SET `modelid_1`='14938' WHERE (`entry`='14878') limit 1;
