7b790ccbbe1edefe72a22da70b89fc57ce17fa69
Flof <flof@nonexistent.fake>
2015-11-02 22:38:00 +0100

    War effort turnin gossip.

--- SQL-post-2015-08-05.sql
+++ SQL-post-2015-08-05.sql
@@ -1593,3 +1593,38 @@ REPLACE INTO `zp_mangosd`.`creature_loot_template` (`entry`, `item`, `ChanceOrQu
 REPLACE INTO `zp_mangosd`.`creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`, `condition_value1`, `condition_value2`) VALUES (11722, 20384, 100, 0, 1, 3, 2, 20402, 1);
 REPLACE INTO `zp_mangosd`.`creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`, `condition_value1`, `condition_value2`) VALUES (11721, 20384, 100, 0, 1, 3, 2, 20402, 1);
 REPLACE INTO `zp_mangosd`.`creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`, `condition_value1`, `condition_value2`) VALUES (11698, 20384, 100, 0, 1, 3, 2, 20402, 1);
+
+-- War effort turnin gossip
+
+REPLACE INTO `zp_mangosd`.`npc_text` (`ID`, `text0_0`, `text0_1`, `lang0`, `prob0`, `em0_0`, `em0_1`, `em0_2`, `em0_3`, `em0_4`, `em0_5`, `text1_0`, `text1_1`, `lang1`, `prob1`, `em1_0`, `em1_1`, `em1_2`, `em1_3`, `em1_4`, `em1_5`, `text2_0`, `text2_1`, `lang2`, `prob2`, `em2_0`, `em2_1`, `em2_2`, `em2_3`, `em2_4`, `em2_5`, `text3_0`, `text3_1`, `lang3`, `prob3`, `em3_0`, `em3_1`, `em3_2`, `em3_3`, `em3_4`, `em3_5`, `text4_0`, `text4_1`, `lang4`, `prob4`, `em4_0`, `em4_1`, `em4_2`, `em4_3`, `em4_4`, `em4_5`, `text5_0`, `text5_1`, `lang5`, `prob5`, `em5_0`, `em5_1`, `em5_2`, `em5_3`, `em5_4`, `em5_5`, `text6_0`, `text6_1`, `lang6`, `prob6`, `em6_0`, `em6_1`, `em6_2`, `em6_3`, `em6_4`, `em6_5`, `text7_0`, `text7_1`, `lang7`, `prob7`, `em7_0`, `em7_1`, `em7_2`, `em7_3`, `em7_4`, `em7_5`) VALUES (8008, 'Greetings, $c! Thank you for helping us with the preparations for the forthcoming war. We need all the donations we can get!', NULL, 0, 0, 0, 0, 0, 0, 0, 0, '', NULL, 0, 0, 0, 0, 0, 0, 0, 0, NULL, NULL, 0, 0, 0, 0, 0, 0, 0, 0, NULL, NULL, 0, 0, 0, 0, 0, 0, 0, 0, NULL, NULL, 0, 0, 0, 0, 0, 0, 0, 0, NULL, NULL, 0, 0, 0, 0, 0, 0, 0, 0, NULL, NULL, 0, 0, 0, 0, 0, 0, 0, 0, NULL, NULL, 0, 0, 0, 0, 0, 0, 0, 0);
+
+REPLACE INTO `zp_mangosd`.`npc_gossip` (`npc_guid`, `textid`) VALUES (12611, 8008);
+REPLACE INTO `zp_mangosd`.`npc_gossip` (`npc_guid`, `textid`) VALUES (12614, 8008);
+REPLACE INTO `zp_mangosd`.`npc_gossip` (`npc_guid`, `textid`) VALUES (12615, 8008);
+REPLACE INTO `zp_mangosd`.`npc_gossip` (`npc_guid`, `textid`) VALUES (12616, 8008);
+REPLACE INTO `zp_mangosd`.`npc_gossip` (`npc_guid`, `textid`) VALUES (12612, 8008);
+REPLACE INTO `zp_mangosd`.`npc_gossip` (`npc_guid`, `textid`) VALUES (12613, 8008);
+REPLACE INTO `zp_mangosd`.`npc_gossip` (`npc_guid`, `textid`) VALUES (12607, 8008);
+REPLACE INTO `zp_mangosd`.`npc_gossip` (`npc_guid`, `textid`) VALUES (12605, 8008);
+REPLACE INTO `zp_mangosd`.`npc_gossip` (`npc_guid`, `textid`) VALUES (12606, 8008);
+REPLACE INTO `zp_mangosd`.`npc_gossip` (`npc_guid`, `textid`) VALUES (12617, 8008);
+REPLACE INTO `zp_mangosd`.`npc_gossip` (`npc_guid`, `textid`) VALUES (12619, 8008);
+REPLACE INTO `zp_mangosd`.`npc_gossip` (`npc_guid`, `textid`) VALUES (12618, 8008);
+REPLACE INTO `zp_mangosd`.`npc_gossip` (`npc_guid`, `textid`) VALUES (12609, 8008);
+REPLACE INTO `zp_mangosd`.`npc_gossip` (`npc_guid`, `textid`) VALUES (12610, 8008);
+REPLACE INTO `zp_mangosd`.`npc_gossip` (`npc_guid`, `textid`) VALUES (12608, 8008);
+REPLACE INTO `zp_mangosd`.`npc_gossip` (`npc_guid`, `textid`) VALUES (12626, 8008);
+REPLACE INTO `zp_mangosd`.`npc_gossip` (`npc_guid`, `textid`) VALUES (12628, 8008);
+REPLACE INTO `zp_mangosd`.`npc_gossip` (`npc_guid`, `textid`) VALUES (12627, 8008);
+REPLACE INTO `zp_mangosd`.`npc_gossip` (`npc_guid`, `textid`) VALUES (12623, 8008);
+REPLACE INTO `zp_mangosd`.`npc_gossip` (`npc_guid`, `textid`) VALUES (12624, 8008);
+REPLACE INTO `zp_mangosd`.`npc_gossip` (`npc_guid`, `textid`) VALUES (12625, 8008);
+REPLACE INTO `zp_mangosd`.`npc_gossip` (`npc_guid`, `textid`) VALUES (12622, 8008);
+REPLACE INTO `zp_mangosd`.`npc_gossip` (`npc_guid`, `textid`) VALUES (12620, 8008);
+REPLACE INTO `zp_mangosd`.`npc_gossip` (`npc_guid`, `textid`) VALUES (12621, 8008);
+REPLACE INTO `zp_mangosd`.`npc_gossip` (`npc_guid`, `textid`) VALUES (12632, 8008);
+REPLACE INTO `zp_mangosd`.`npc_gossip` (`npc_guid`, `textid`) VALUES (12634, 8008);
+REPLACE INTO `zp_mangosd`.`npc_gossip` (`npc_guid`, `textid`) VALUES (12633, 8008);
+REPLACE INTO `zp_mangosd`.`npc_gossip` (`npc_guid`, `textid`) VALUES (12629, 8008);
+REPLACE INTO `zp_mangosd`.`npc_gossip` (`npc_guid`, `textid`) VALUES (12630, 8008);
+REPLACE INTO `zp_mangosd`.`npc_gossip` (`npc_guid`, `textid`) VALUES (12631, 8008);
