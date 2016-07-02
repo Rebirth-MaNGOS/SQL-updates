f401165d8cadb459e2d5a1c83d83bd7b5b71091d
Flof <flof@nonexistent.fake>
2016-07-02 21:57:57 +0200

    New scripts for AQ40 trash

---
+++ SQL-post-2016-06-21.sql
@@ -0,0 +1,3 @@
+UPDATE `zp_mangosd`.`creature_template` SET `ScriptName`='mob_qiraji_mindslayer' WHERE  `entry`=15246 LIMIT 1;
+UPDATE `zp_mangosd`.`creature_template` SET `ScriptName`='mob_qiraji_slayer' WHERE  `entry`=15250 LIMIT 1;
+UPDATE `zp_mangosd`.`creature_template` SET `ScriptName`='mob_qiraji_champion' WHERE  `entry`=15252 LIMIT 1;
