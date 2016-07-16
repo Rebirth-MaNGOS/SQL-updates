d1c3af57cb34f91670d50ca35ad99e96c4d03168
muggle <muggle@therebirth.net>
2016-07-16 21:54:36 +0200

    Corrected required levels for Enchanted Thorium Platemail quests, volume I-III

---
+++ SQL-post-2016-07-16.sql
@@ -0,0 +1,4 @@
+-- Corrected required levels for Enchanted Thorium Platemail quests, volume I-III
+UPDATE `zp_mangosd`.`quest_template` SET `MinLevel`='52' WHERE (`entry`='7649');
+UPDATE `zp_mangosd`.`quest_template` SET `MinLevel`='58' WHERE (`entry`='7650');
+UPDATE `zp_mangosd`.`quest_template` SET `MinLevel`='60' WHERE (`entry`='7651');
