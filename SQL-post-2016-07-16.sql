8232266ba5121d8dabdb4c16f2f41094ecad0ac7
Revontulet <revontulet@nonexistent.fake>
2016-08-06 11:42:17 +0200

    Corruptors scourgestones quest fixed typo

--- SQL-post-2016-07-16.sql
+++ SQL-post-2016-07-16.sql
@@ -14,3 +14,8 @@ UPDATE `zp_mangosd`.`quest_template` SET `SpecialFlags`='1' WHERE (`entry`='8829
 
 -- Quest: Stealing Supplies should now award correct amount of silver star item
 UPDATE `zp_mangosd`.`quest_template` SET `RewChoiceItemCount3`=200 WHERE  `entry`=1370;
+
+-- Corruptor's Scourgestones fixed typo
+UPDATE `zp_mangosd`.`quest_template` SET `OfferRewardText`='Congratulations, $N - I am pleased to award you with an Argent Dawn valor token!\r\n\r\nTo acquire a valor token in such a manner indicates that you are a true hero in the cause of good. We value all effort brought to bear against the Scourge, but to take down one of their leaders is to truly deliver onto them a crushing defeat!\r\n\r\nFor the Dawn, my $G brother:sister;!' WHERE  `entry`=5508;
+UPDATE `zp_mangosd`.`quest_template` SET `OfferRewardText`='Congratulations, $N - I am pleased to award you with an Argent Dawn valor token!\r\n\r\nTo acquire a valor token in such a manner indicates that you are a true hero in the cause of good. We value all effort brought to bear against the Scourge, but to take down one of their leaders is to truly deliver onto them a crushing defeat!\r\n\r\nFor the Dawn, my $G brother:sister;!' WHERE  `entry`=5404;
+UPDATE `zp_mangosd`.`quest_template` SET `OfferRewardText`='Congratulations, $N - I am pleased to award you with an Argent Dawn valor token!\r\n\r\nTo acquire a valor token in such a manner indicates that you are a true hero in the cause of good. We value all effort brought to bear against the Scourge, but to take down one of their leaders is to truly deliver onto them a crushing defeat!\r\n\r\nFor the Dawn, my $G brother:sister;!' WHERE  `entry`=5406;
