d9d8f6aeaaf74da63f81b464976dba65c0014d16
muggle <muggle@therebirth.net>
2016-09-10 19:37:11 +0200

    ZF, change of walking/running speed of trolls

---
+++ SQL-post-2016-09-10.sql
@@ -0,0 +1,2 @@
+-- change the walking/running speed of trolls in ZF
+UPDATE `zp_mangosd`.`creature_template` SET `speed_walk`='1.32' where entry in ('5649','7246','5648','10082','7247','7272','5650','7274','7795','7267','7797','7271','8127');
