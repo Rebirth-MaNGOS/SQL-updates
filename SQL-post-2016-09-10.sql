89b86a5c0fbe95a048b407e67d8d500008e92f90
Revontulet <revontulet@nonexistent.fake>
2016-09-13 15:52:36 +0200

    Giant eye tentacle size and damage

--- SQL-post-2016-09-10.sql
+++ SQL-post-2016-09-10.sql
@@ -1,2 +1,5 @@
 -- change the walking/running speed of trolls in ZF
 UPDATE `zp_mangosd`.`creature_template` SET `speed_walk`='1.32' where entry in ('5649','7246','5648','10082','7247','7272','5650','7274','7795','7267','7797','7271','8127');
+
+-- Giant Eye Tentacle updated damage to same as giant claw and size
+UPDATE `zp_mangosd`.`creature_template` SET `mindmg`=3800, `maxdmg`=4000, `scale`=3 WHERE  `entry`=15334;
