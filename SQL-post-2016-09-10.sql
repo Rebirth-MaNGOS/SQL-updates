b10b9f2952ff0a069cf0575a4372457a83a69a32
Revontulet <revontulet@nonexistent.fake>
2016-09-21 16:39:35 +0200

    Huntsman radley removed duplicate wolf and blood of heroes added traps

--- SQL-post-2016-09-10.sql
+++ SQL-post-2016-09-10.sql
@@ -6,3 +6,10 @@ UPDATE `zp_mangosd`.`creature_template` SET `mindmg`=3800, `maxdmg`=4000, `scale
 
 -- Thaddius spell
 REPLACE INTO `zp_mangosd`.`spell_script_target` VALUES (28159, 1, 15928);
+
+-- Huntsman Radley remove duplicate wolf spawn
+DELETE FROM `zp_mangosd`.`creature_ai_scripts` WHERE  `id`=1161302;
+
+-- Add traps to Blood of Heroes
+REPLACE INTO `zp_mangosd`.`gameobject` VALUES (1272, 176214, 0, 2192.74, -4479.21, 87.66, 3.03687, 0, 0, 0.99863, 0.052336, 1200, 100, 1);
+REPLACE INTO `zp_mangosd`.`gameobject` VALUES (1273, 176214, 0, 2888.38, -4086.29, 101.381, -2.19912, 0, 0, 0.891007, -0.45399, 1200, 100, 1);
