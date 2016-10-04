f8209ff830134945b194d827c157168b373f5569
Revontulet <revontulet@nonexistent.fake>
2016-10-04 14:27:18 +0200

    Kelthuzad adds speed updated

--- SQL-post-2016-09-28.sql
+++ SQL-post-2016-09-28.sql
@@ -45,3 +45,15 @@ REPLACE INTO `zp_mangosd`.`creature_onkill_reputation` VALUES (15250, 910, 0, 3,
 REPLACE INTO `zp_mangosd`.`creature_onkill_reputation` VALUES (15252, 910, 0, 3, 0, 100, 0, 0, 0, 0);
 REPLACE INTO `zp_mangosd`.`creature_onkill_reputation` VALUES (15312, 910, 0, 3, 0, 100, 0, 0, 0, 0);
 REPLACE INTO `zp_mangosd`.`creature_onkill_reputation` VALUES (15311, 910, 0, 3, 0, 100, 0, 0, 0, 0);
+
+-- Soldier of the Frozen Wastes speed
+UPDATE `zp_mangosd`.`creature_template` SET `speed_walk`=0.25, `speed_run`=0.285715 WHERE  `entry`=16427;
+
+-- Unstoppable Abomination
+UPDATE `zp_mangosd`.`creature_template` SET `speed_walk`=0.7, `speed_run`=0.800002 WHERE  `entry`=16428;
+
+-- Soul Weaver
+UPDATE `zp_mangosd`.`creature_template` SET `speed_walk`=0.1, `speed_run`=0.114286 WHERE  `entry`=16429;
+
+-- Shadow Fissure
+UPDATE `zp_mangosd`.`creature_template` SET `AIName`='', `ScriptName`='npc_shadow_fissure' WHERE  `entry`=16129;
