6559d41c71165efe8d9c1414e45de2667c60d69e
Revontulet <revontulet@nonexistent.fake>
2016-10-22 11:48:21 +0200

    Cursed justicar updated stats

--- SQL-post-2016-10-08.sql
+++ SQL-post-2016-10-08.sql
@@ -11,3 +11,7 @@ UPDATE `zp_mangosd`.`item_template` SET `spellcharges_1`=0 WHERE  `entry`=19046;
 UPDATE `zp_mangosd`.`item_template` SET `spellcharges_1`=0, `spellcategory_1`=1153, `spellcategorycooldown_1`=120000 WHERE  `entry`=19045;
 UPDATE `zp_mangosd`.`item_template` SET `spellcharges_1`=0 WHERE  `entry`=18607;
 UPDATE `zp_mangosd`.`item_template` SET `spellcharges_1`=0 WHERE  `entry`=18606;
+
+-- Cursed Justicar updated stats
+UPDATE `zp_mangosd`.`creature_template` SET `minlevel`=57, `maxlevel`=58, `minhealth`=6891, `maxhealth`=7107, `armor`=3799, `rank`=1, `mindmg`=406, `maxdmg`=495, `attackpower`=250 WHERE  `entry`=7072;
+
